b2d834488670219621038d0d13f4061677d34696
bartek3774 <bartek.stempien@poczta.onet.pl>
2023-04-14 11:39:05 +0200

    Create log.groovy

---
+++ vars/log.groovy
@@ -0,0 +1,21 @@
+def loadColor(){
+    RED='\033[0;31m'
+    CYAN='\033[0;36m'
+    BLUE='\033[0;34m'
+    ON_YELLOW = '\033[43m'
+    NC ='\033[0m'
+}
+def info(message) {
+    loadColor()
+    sh """set +x; echo "${BLUE}INFO: $message ${NC}" """
+}
+
+def warning(message) {
+    loadColor()
+    sh """set +x; echo "${ON_YELLOW}WARNING: $message ${NC}" """
+}
+
+def error(message) {
+    loadColor()
+    sh"""set +x; echo "${RED}ERROR: $message ${NC}" """
+}
